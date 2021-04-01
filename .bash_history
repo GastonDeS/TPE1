ba55b96e0844128196af3023a0c0bae507b0e5ef
felip <fcupito@itba.edu.ar>
2021-04-01 12:24:54 -0300

    salida del slave.c

---
+++ .bash_history
@@ -0,0 +1,86 @@
+gcc -g -Wall slace.c -o slave
+gcc -g -Wall slave.c -o slave
+.salve
+./slave
+apt-get install minisat
+./slave
+make all
+make clean
+make all
+./solve 
+make clean
+./solve 
+make all
+./solve 
+./solve 
+make clean
+make all
+./solve 
+apt-get install minisat
+make all
+./solve 
+\ll
+ls
+sat
+minisat
+la
+ls
+apt install minisat
+cd
+la
+ls
+ls
+make clean
+make all
+./slave
+./solve
+make all
+make clean
+make all
+./solve
+make clean
+make all
+./solve
+make clean
+make all
+./solve
+valgrind ./solve
+valgrind -v ./solve
+clear
+ls
+make clean
+make all
+./solve
+make clean
+make all
+./solve
+strace ./solve
+strace -o 1 ./solve
+make clean
+make all
+strace -o 1 ./solve
+make clean
+make all
+strace -o 1 ./solve
+make clean
+make all
+strace -o 1 ./solve
+./solve
+make clean
+make all
+./solve
+ps
+ps -l
+claer
+clean
+claer
+clean
+clear
+make clear
+make clean
+make all
+./solve
+strace -ff -o out ./solve
+make clean
+make all
+strace -ff -o out ./solve
--- slave.c
+++ slave.c
@@ -7,25 +7,32 @@
 int main(int argc, char const *argv[]) { 
 
     char const minisat[] = {"minisat %s | grep -o -e \"Number of.*[0-9]\\+\" -e \"CPU time.*\" -e \".*SATISFIABLE\""};
-    int minisat_size = (sizeof(minisat)/sizeof(minisat[0]) -2); //le saco %s
-    
+    int minisat_size = (sizeof(minisat)/sizeof(minisat[0]) -2); //calcula la dimencion del minsisat sin el %s
     char *buff = NULL;
-    size_t buffSize = 0;
-    size_t result;
+    int buffSize = 0;
+    int result;
 
-    while ((result = getline(&buff, &buffSize, stdin)) > 0){
-        char command[minisat_size+result-1];
+    while ((result = getline(&buff, &buffSize, stdin)) > 1){ 
+        char command[minisat_size+result-1]; 
         buff[result-1] = 0;
-        
         sprintf(command, minisat, buff);
-        
         char *const params[] = {command, NULL};
-        // char *const params[] = {"minisat ./files/pigeon-hole/hole7.cnf |  grep -o -e \"Number of.*[0-9]\\+\" -e \"CPU time.*\" -e \".*SATISFIABLE\"", NULL};
+        
+        printf("%s",buff);
         FILE * fp = popen(*params, "w");
-        pclose(fp);
+        printf("%s",getpid());
 
+        pclose(fp);
+        buff = NULL;
     }
- 
+    
+    free(buff);
+    
+    if(result < 0){
+        perror("getline");
+        exit(-1);
+    }
+
     return 0;
 }
 
