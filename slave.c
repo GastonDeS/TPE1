b8d5c58ccd513b4bd15ef20d720f1a75b8b8c9ed
felip <fcupito@itba.edu.ar>
2021-04-02 02:19:46 -0300

    comunicacion slave-solve

--- slave.c
+++ slave.c
@@ -3,51 +3,66 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <errno.h>
 #include <stdlib.h>
 
-int checkCNF(char *path);
+int checkCNF(const char *path);
 
 int main(int argc, char const *argv[]) {
 
     char const minisat[] = {"minisat %s | grep -o -e \"Number of.*[0-9]\\+\" -e \"CPU time.*\" -e \".*SATISFIABLE\""};
-    int minisat_size = (sizeof(minisat)/sizeof(minisat[0]) -2); //calcula la dimencion del minsisat sin el %s
-    char *buff = NULL;
-    size_t buffSize = 0;
-    int result;
+    int minisat_size = (sizeof(minisat)/sizeof(minisat[0]) -2); //la dimencion de minisat sin %s
+    char *fileName = NULL;
+    size_t fileNameSize = 0;
+    size_t numChar;
 
-    while ((result = getdelim(&buff, &buffSize, '\n',stdin)) > 0){ //esta bien que sea mayor que uno
-        fwrite(buff,buffSize,1,stdout);
-        putchar('\n');
-        char command[minisat_size+result-1];
-        printf("result: %d\n", result);
-        buff[result-1] = 0;
-        sprintf(command, minisat, buff);
-        char *const params[] = {command, NULL};
+    while ((numChar = getdelim(&fileName, &fileNameSize, '\n',stdin)) > 0){ 
         
+        //preparacion de parametros para minisat
+        char command[minisat_size+numChar-1];
+        fileName[numChar-1] = 0; //le saco el salto de linea
+        checkCNF(fileName);//si no existe el archivo o no se permite la lectura termina la ejecucion 
+        sprintf(command, minisat, fileName);
+        char *const params[] = {command, NULL};
+    
+        //minisat
         FILE * fp;
-        if (( fp = popen(*params, "w")) == NULL ){
+        char *minisatReturn = NULL;
+        size_t minisatReturnSize = 0;
+        size_t minisatReturnDim;
+        if (( fp = popen(*params, "r")) == NULL ){
             perror("popen");
             exit(-1);
         }
-        printf("%d\n",getpid());
+        minisatReturnDim = getdelim(&(minisatReturn), &minisatReturnSize,'\0', fp);
+
+        //get pid
+        char pid[10]; // Mejorar!!!
+        int pidDim ;
+        pidDim = sprintf(pid, "%d\n", getpid());
+
+        //imprimir resultado final
+        fileName[numChar-1] = '\n'; //le vuelvo agregar el salo de linea
+        strcat(fileName, minisatReturn);
+        strcat(fileName,pid);
+        write(1, fileName, (minisatReturnDim+numChar+pidDim));
 
         pclose(fp);
+        free(minisatReturn);
     }
 
-    free(buff);
-
-    if(result < 0){
+    if(numChar == -1 && (errno == EINVAL || errno == ENOMEM)){
         perror("getline");
         exit(-1);
     }
 
+    free(fileName);
     return 0;
 }
 
-int checkCNF(char *path){
-    struct stat buf;
-    int valid=1;
-    stat(path,&buf);
-    valid = !S_ISDIR(buf.st_mode);
-    return valid;
+int checkCNF(const char *path){
+    if(access(path, R_OK) == 0)
+        return 0;
+    perror("invalid path");
+    exit(-1);
 }
--- solve.c
+++ solve.c
@@ -7,7 +7,104 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
+#define NUM_CHILD 8
+#define READ 0
+#define WRITE 1
 
+int main(int argc, char **argv){
+
+    //int child = (NUM_CHILD<argc)?NUM_CHILD:argc;
+    //child=4;
+    int ppid = getpid();
+    char *const *argvs = NULL;
+    int fd[NUM_CHILD][2];
+    int pidC[NUM_CHILD];
+
+    int i;
+    for (i = 0; i < NUM_CHILD; i++){
+        int fdFatherToSon[2]; //father(1) ---> son(0)
+        int fdSonToFather[2]; // son(1) ---> father(0)
+        if (pipe(fdFatherToSon) < 0 || pipe(fdSonToFather) < 0){ 
+            perror("pipe");
+            exit(-1);
+        }
+
+        pid_t pid = fork();
+        if (pid < 0){
+            perror("fork");
+            exit(-1);
+        }
+        if (pid == 0){  //hijo
+            close(fdFatherToSon[WRITE]); //cierro los fd correspondientes al padre
+            close(fdSonToFather[READ]);
+            close(1); //cierro los std fd
+            close(0);
+            if (dup2(fdFatherToSon[READ], 0) < 0 || dup2(fdSonToFather[WRITE], 1) < 0)
+            { // la entrada de escritura del pipe quedo conetada con el stdout
+                perror("dup2");
+                exit(-1);
+            }
+            close(fdFatherToSon[READ]); 
+            close(fdSonToFather[WRITE]); 
+
+            execv("./slave", NULL); 
+            perror("execv");
+            exit(-1);
+        }
+        else{   //padre
+            close(fdSonToFather[WRITE]); //cierro los fd correspondientes al hijo
+            close(fdFatherToSon[READ]);
+
+            fd[i][READ] = fdSonToFather[READ];
+            fd[i][WRITE] = fdFatherToSon[WRITE];
+
+            pidC[i] = pid;
+        }
+    }
+
+    //asignacion de la primera tanda de archivos
+    for (i = 0; i < NUM_CHILD; i++){
+        if (write(fd[i][WRITE], "hole6.cnf\n", 10) < 0)
+            perror("write");
+    }
+
+    //loop
+    while (1){ // mientras haya cnf para analisar
+        fd_set readfds;
+        FD_ZERO(&readfds);
+        int i;
+        for (i = 0; i < NUM_CHILD; i++){
+            FD_SET(fd[i][READ], &readfds);
+        }
+
+        int ready = select(fd[NUM_CHILD-1][READ]+1, &readfds, NULL, NULL, NULL);
+
+        if (ready == -1) {  //compruebo errores del select
+            perror("select()");
+            exit(EXIT_FAILURE);
+        }
+        
+        for (i = 0; i < NUM_CHILD && ready > 0; i++){ 
+            if(FD_ISSET(fd[i][READ], &readfds) != 0){
+                
+                //prueba de funcionamiento--------------------------------
+                char buff[512];
+                read(fd[i][READ], buff, sizeof(buff));
+                printf("%s \n", buff);
+
+                //----------------------------------------------------------
+                
+                //lee el resultado 
+                //le mando el proximo archivo, sino cerrar el pipe
+                ready--;  
+            }
+        }
+    }
+    return 0;
+}
+
+
+/*
 #define CHILD 8
 
 void readAndPrint(int fd);
@@ -107,3 +204,5 @@ void readAndPrint(int fd){ // recibe el fd a leer
         fwrite(line, linelen, 1, stdout);
     }
 }
+
+*/
